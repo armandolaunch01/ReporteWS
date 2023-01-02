d616754d85b5db23abd5311215d59184d8b322dc
Armando <lx03136@innovaccion.mx>
2023-01-02 17:56:55 -0600

    index.js modified

--- index.js
+++ index.js
@@ -5,39 +5,48 @@ const nodemailer = require('nodemailer');
 //Creating an instance of express
 app = express();
 
-//
-let mailOptions = {
-   from: 'ing.armando.silverio@gmail.com',
-    to: 'armando.silverio@webmaps.com.mx',
-    subject: 'This is a scheduled email testing',
-    text: "This is very simple email testing",
-    attachments: [{
-        filename: 'WS' + ".xlsx",
-        path: __dirname + '/WS.xlsx'
-    }],
-    html: '<b>THE HTML CONTENT</b>'
-}
-
-//Mail transport  configuration
-let transporter = nodemailer.createTransport({
-   service: 'gmail',
-    auth: {
-        user: 'ing.armando.silverio@gmail.com',
-        pass: 'wwrgyvkswkldwdls',
-    },
-    tls: {
-        rejectUnauthorized: false
-    },
-});
-
 
 cron.schedule('*/5 * * * * *', () => {  
-console.log('----------------------------------------');
-console.log("Tasked scheduled with 1 minute interval");
-console.log('Running cron process');
-});
-//Delivering mail with sendMail method 
+    console.log('----------------------------------------');
+    console.log("Tasked scheduled with 5 seconds interval");
+    console.log('Running cron process');
 
+    //
+let mailOptions = {
+    from: 'ing.armando.silverio@gmail.com',
+     to: 'armando.silverio@webmaps.com.mx',
+     subject: 'This is a scheduled email testing',
+     text: "This is very simple email testing",
+     attachments: [{
+         filename: 'WS' + ".xlsx",
+         path: __dirname + '/WS.xlsx'
+     }],
+     html: '<b>THE HTML CONTENT</b>'
+ }
+ 
+ //Mail transport  configuration
+ let transporter = nodemailer.createTransport({
+    service: 'gmail',
+     auth: {
+         user: 'ing.armando.silverio@gmail.com',
+         pass: 'wwrgyvkswkldwdls',
+     },
+     tls: {
+         rejectUnauthorized: false
+     },
+ });
+ 
+ 
+ 
+ //Delivering mail with sendMail method 
+ transporter.sendMail(mailOptions, (error, info) => {
+     if (error) console.log(error);
+     else console.log('Email sent: ' + info.response);
+ });
+    });
+
+
+//port hearing
 var port = 3000;
  app.listen(port, (res, send) =>{
     console.log(`listening on port ${port}`);
